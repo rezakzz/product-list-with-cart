4512db7670557278b01538f7a52cb34fcffff94b
rezakzz <rezafkz2006@gmail.com>
2026-05-22 14:53:56 +0700

    Implement basic add to cart functionality

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-const cartButtons = document.querySelectorAll(".cart-btn");
+const buttons = document.querySelectorAll(".cart-btn");
+const cartItems = document.getElementById("cart-items");
 const cartCount = document.getElementById("cart-count");
+const emptyImage = document.getElementById("empty-cart-image");
+const emptyText = document.getElementById("empty-cart-text");
 
-let totalItems = 0;
+let cart = [];
 
-cartButtons.forEach((button) => {
+buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        totalItems++;
-        cartCount.textContent = totalItems;
+        const productCard = button.closest(".product-card");
+
+        const name = productCard.dataset.name;
+        const price = productCard.dataset.price;
+
+        cart.push({ name, price });
+
+        updateCart();
     });
 });
+
+function updateCart() {
+    cartCount.textContent = cart.length;
+    cartItems.innerHTML = "";
+
+    if (cart.length > 0) {
+        emptyImage.style.display = "none";
+        emptyText.style.display = "none";
+    }
+
+    cart.forEach((item) => {
+        cartItems.innerHTML += `
+            <div class="cart-item">
+                <h4>${item.name}</h4>
+                <p>$${item.price}</p>
+            </div>
+        `;
+    });
+}
