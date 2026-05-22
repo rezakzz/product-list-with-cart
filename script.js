0b650cbf73b30f687ad17a66de0ceb97501c46ba
rezakzz <rezafkz2006@gmail.com>
2026-05-22 15:24:52 +0700

    Improve cart UI and quantity controls

--- script.js
+++ script.js
@@ -9,18 +9,27 @@ let cart = [];
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         const productCard = button.closest(".product-card");
-
         const name = productCard.dataset.name;
-        const price = productCard.dataset.price;
+        const price = Number(productCard.dataset.price);
 
-        cart.push({ name, price });
+        const existingItem = cart.find((item) => item.name === name);
+
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            cart.push({ name, price, quantity: 1 });
+            productCard.classList.add("active");
+        }
 
         updateCart();
+        updateProductButton(productCard, name);
     });
 });
 
 function updateCart() {
-    cartCount.textContent = cart.length;
+    const totalQuantity = cart.reduce((total, item) => total + item.quantity, 0);
+    cartCount.textContent = totalQuantity;
+
     cartItems.innerHTML = "";
 
     if (cart.length > 0) {
@@ -31,9 +40,36 @@ function updateCart() {
     cart.forEach((item) => {
         cartItems.innerHTML += `
             <div class="cart-item">
-                <h4>${item.name}</h4>
-                <p>$${item.price}</p>
-            </div>
+        <h4>${item.name}</h4>
+
+        <div class="cart-details">
+            <span class="quantity">${item.quantity}x</span>
+            <span class="unit-price">@ $${item.price.toFixed(2)}</span>
+            <span class="total-price">$${(item.price * item.quantity).toFixed(2)}</span>
+        </div>
+    </div>
         `;
     });
+}
+
+function updateProductButton(productCard, name) {
+    const item = cart.find((item) => item.name === name);
+    const imageBox = productCard.querySelector(".image-box");
+
+    imageBox.querySelector(".cart-btn")?.remove();
+    imageBox.querySelector(".quantity-btn")?.remove();
+
+    imageBox.innerHTML += `
+        <div class="quantity-btn">
+            <button class="qty-control minus">
+                <img src="./assets/images/icon-decrement-quantity.svg" alt="minus">
+            </button>
+
+            <span>${item.quantity}</span>
+
+            <button class="qty-control plus">
+                <img src="./assets/images/icon-increment-quantity.svg" alt="plus">
+            </button>
+        </div>
+    `;
 }
